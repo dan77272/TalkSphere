abf056c933b904812035eb14c4a452c10dba7ee1
dan77272 <danysanioura@gmail.com>
2026-02-16 21:02:15 -0500

    update dependencies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ export default function Navbar({usersOnline}) {
     return (
         <div className='flex justify-between items-center shadow-custom h-auto'>
             <div className="flex md:flex-row flex-col items-center md:gap-16 mx-4">
-                <p className="text-[50px] m-2 font-bold">TalkSphere</p>
+                <p data-testid="logo" className="text-[50px] m-2 font-bold">TalkSphere</p>
                 <p className="text-[20px]">Talk to Strangers!</p>
             </div>
             <div className="mx-4">
-                <p className="text-[15px] md:text-[25px]">{usersOnline || 0} online now</p>
+                <p data-testid="online-count" className="text-[15px] md:text-[25px]">{usersOnline || 0} online now</p>
             </div>
         </div>
     );
